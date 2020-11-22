271f915252fae00c9d9672c81b72e237222f6b84
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-22 15:34:21 +0300

    added upload view function and template

--- instagram/models.py
+++ instagram/models.py
@@ -1,19 +1,23 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+from django.utils import timezone
 
 # Create your models here.
 class Image(models.Model):
     image = CloudinaryField('image',null=False,default=True)
     title = models.CharField(max_length=500,null=True)
-    caption = models.CharField(max_length=3000,null=True)
+    caption = models.TextField(null=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    
+    date_posted = models.DateTimeField(default=timezone.now)
     likes=models.IntegerField(null=True, default=0)
     comment = models.CharField(max_length=300,null=True)
 
     def __str__(self):
         return self.title
+
+    class Meta:
+        ordering = ['-date_posted']
 
 
 class Profile(models.Model):
--- instagram/views.py
+++ instagram/views.py
@@ -3,7 +3,17 @@ from .models import Image
 
 
 
+
 # Create your views here.
 def index(request):
     posts = Image.objects.all()
     return render(request,'instagram/index.html',{'posts':posts})
+
+
+def uploadImage(request):
+    return render(request,'instagram/upload.html')
+
+
+
+    
+    
