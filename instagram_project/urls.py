9f46d42279cd5116aafcc86f0c178f2448801653
kipkoech-msojo <collinskipkoech95@gmail.com>
2020-11-22 11:56:17 +0300

    added functionality to allow user to upload profile picture

--- instagram_project/urls.py
+++ instagram_project/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
 from users import views as user_views
 
@@ -28,3 +30,6 @@ urlpatterns = [
     path('profile/',user_views.profile, name='profile'),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
